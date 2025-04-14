1bf77ebecc1a02c0187d6161e5d4e965abeb9c2c
Isabel Beck <beck.isabel42@gmail.com>
2025-04-13 21:04:14 -0400

    added info cards to main page

--- web-app/src/components/Welcome.tsx
+++ web-app/src/components/Welcome.tsx
@@ -1,21 +1,37 @@
 'use client';
 import Link from 'next/link';
+import Image from 'next/image';
+import Pic1 from '../assets/recordswHeadphones.jpg';
+import Pic2 from '../assets/coffee.jpg';
+import Pic3 from '../assets/girlListening.jpg';
 
 const Welcome = () => { 
     
     return (
-        <div className="min-h-screen flex items-center justify-center">
-        <div className="bg-white bg-opacity-80 p-8 rounded-lg shadow-md max-w-md w-full text-center">
-        <h1 className="text-2xl font-bold text-gray-800">Welcome to UGA Items</h1>
-        <div className="mt-12">
-            <Link
-                     href="/show-items"
-                     className="inline-block px-4 py-2 bg-red-600 text-white hover:bg-red-700 transition rounded"
-          >
-                     Show Item List
-        </Link>
-        </div>
-        </div>
+        <div className="min-h-full flex items-center justify-center">
+            <div className="bg-white bg-opacity-80 p-8 rounded-lg shadow-md max-w-md w-full text-center m-4">
+                <Image src={Pic1} alt="records and headphones" className='w-max justify-self-center p-5'/>
+            <h1 className="text-xl font-bold text-gray-800 text-left">Leaderboard</h1>
+            <div className="mt-12">
+                <p className='text-left'>See how your listening habits compare to your fellow users</p>
+            </div>
+
+            </div>
+            <div className="bg-white bg-opacity-80 p-8 rounded-lg shadow-md max-w-md w-full text-center m-4">
+            <Image src={Pic2} alt="coffee cup" className='w-max justify-self-center p-5'/>
+            <h1 className="text-xl font-bold text-gray-800 text-left">Connect with Friends</h1>
+            <div className="mt-12">
+            <p className='text-left'>Show off your amazing music taste and compare with friends</p>
+            </div>
+
+            </div>
+            <div className="bg-white bg-opacity-80 p-8 rounded-lg shadow-md max-w-md w-full text-center m-4">
+            <Image src={Pic3} alt="girl listening to music" className='w-max justify-self-center p-5'/>
+            <h1 className="text-xl font-bold text-gray-800 text-left">Make playlists</h1>
+            <div className="mt-12">
+            <p className='text-left'>Create new playlists based on your unique tastes and share them with others</p>
+            </div>
+            </div>
     </div>
     )};
 export default Welcome;
